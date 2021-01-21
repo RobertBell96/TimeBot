a0c49455d8e92a093d8e71b15906d9c9f9b71f29
metruzanca <metruzanca@gmail.com>
2021-01-21 17:11:05 +0100

    Logs and better file structure

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 const Discord = require('discord.js');
 require('dotenv').config()
 
-const client = new Discord.Client();
 
-client.once('ready', () => {
-  console.log('TimeBot Online!');
-  client.user.setPresence({ activity: { name: 'you.', type: "WATCHING" }, status: 'online' });
-
-  let currentTimeBeforeChange = getCurrentTime();
-  changeTime(currentTimeBeforeChange);
-
-  setInterval(() => {
-    if(getCurrentTime() != currentTimeBeforeChange)
-    {
-      changeTime(getCurrentTime());
-      currentTimeBeforeChange = getCurrentTime();
-    }
-  }, 1000);
-});
 
 function getCurrentTime() {
   const hms = new Date(Date.now()).toUTCString().split(" ")[4].split(":")
@@ -25,16 +9,45 @@ function getCurrentTime() {
   return currentTime;
 }
 
-function changeTime(time) {
+function changeTime(client, time) {
   client.guilds.cache.get("233603103897812993").members.cache.get(client.user.id).setNickname(time + " ST");
   console.log("Changed time to: " + time);
 }
 
-client.on('message', message => {});
+function main() {
+  const client = new Discord.Client();
 
-try {
-  client.login(process.env.DISCORD_BOT_TOKEN);
-} catch (error) {
-  console.log(error);  
+  client.once('ready', () => {
+    console.log('TimeBot Online!');
+    client.user.setPresence({ activity: { name: 'you.', type: "WATCHING" }, status: 'online' });
+
+    let currentTimeBeforeChange = getCurrentTime();
+    changeTime(client, currentTimeBeforeChange);
+
+    setInterval(() => {
+      if(getCurrentTime() != currentTimeBeforeChange)
+      {
+        changeTime(client, getCurrentTime());
+        currentTimeBeforeChange = getCurrentTime();
+      }
+    }, 10000);
+  });
+
+  client.on('message', message => {});
+
+  try {
+    client.login(process.env.DISCORD_BOT_TOKEN);
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
+}
+
+const token = process.env.DISCORD_BOT_TOKEN
+
+if(token){
+  main()
+} else {
+  console.error("Bot Token not defined");
 }
 

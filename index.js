60fb673ef7bae4f0d6c0500c2932b6edf516e0be
metruzanca <metruzanca@gmail.com>
2021-01-21 16:37:53 +0100

    Added useful logging

--- index.js
+++ index.js
@@ -4,33 +4,37 @@ require('dotenv').config()
 const client = new Discord.Client();
 
 client.once('ready', () => {
-  console.log('Ready!');
+  console.log('TimeBot Online!');
   client.user.setPresence({ activity: { name: 'you.', type: "WATCHING" }, status: 'online' });
 
-  var currentTimeBeforeChange = getCurrentTime();
+  let currentTimeBeforeChange = getCurrentTime();
   changeTime(currentTimeBeforeChange);
 
   setInterval(() => {
     if(getCurrentTime() != currentTimeBeforeChange)
     {
-        changeTime(getCurrentTime());
-        currentTimeBeforeChange = getCurrentTime();
+      changeTime(getCurrentTime());
+      currentTimeBeforeChange = getCurrentTime();
     }
   }, 1000);
 });
 
-function getCurrentTime()
-{
-    var hms = new Date(Date.now()).toUTCString().split(" ")[4].split(":")
-    currentTime = hms[0] + ":" + hms[1];
-    return currentTime;
+function getCurrentTime() {
+  const hms = new Date(Date.now()).toUTCString().split(" ")[4].split(":")
+  currentTime = hms[0] + ":" + hms[1];
+  return currentTime;
 }
 
-function changeTime(time)
-{
-    client.guilds.cache.get("233603103897812993").members.cache.get(client.user.id).setNickname(time + " ST");
+function changeTime(time) {
+  client.guilds.cache.get("233603103897812993").members.cache.get(client.user.id).setNickname(time + " ST");
+  console.log("Changed time to: " + time);
 }
 
 client.on('message', message => {});
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+try {
+  client.login(process.env.DISCORD_BOT_TOKEN);
+} catch (error) {
+  console.log(error);  
+}
+
